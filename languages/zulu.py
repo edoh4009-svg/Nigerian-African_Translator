aa81ffca4141e1c86beb0984eef0a68e837f8d3e
Godwin Edoh <edoh4009@gmail.com>
2025-12-11 22:00:42 +0100

    Add Zulu dictionary with basic translations

--- languages/zulu.py
+++ languages/zulu.py
@@ -1,1 +1,56 @@
+# Zulu Dictionary
+# Created by: [Edoh Godwin]
+# Team Project: African Language Translator
+
+zulu_dict = {
+    # ========== GREETINGS & BASICS (6 words) ==========
+    "hello": "sawubona",
+    "goodbye": "hamba kahle",
+    "thank you": "ngiyabonga",
+    "please": "ngicela",
+    "yes": "yebo",
+    "no": "cha",
+
+    # ========== FAMILY & PEOPLE (4 words) ==========
+    "mother": "umama",
+    "father": "ubaba",
+    "child": "ingane",
+    "friend": "umngane",
+
+    # ========== COMMON OBJECTS (4 words) ==========
+    "water": "amanzi",
+    "food": "ukudla",
+    "house": "indlu",
+    "book": "incwadi",
+
+    # ========== NATURE & ENVIRONMENT (4 words) ==========
+    "sun": "ilanga",
+    "moon": "inyanga",
+    "tree": "isihlahla",
+    "river": "umfula",
+
+    # ========== TIME & DAYS (2 words) ==========
+    "day": "usuku",
+    "night": "ubusuku",
+
+    # ========== BONUS WORDS (if you want more than 20) ==========
+    "how are you": "unjani",
+    "good morning": "sawubona",
+    "love": "uthando",
+    "school": "isikole",
+    "work": "umsebenzi"
+}
+
+# Dictionary information
+TOTAL_WORDS = len(zulu_dict)
+LANGUAGE_NAME = "isiZulu"
+CREATED_BY = "[Edoh Godwin]"
+
+# Optional: Add pronunciation helper
+pronunciation_guide = {
+    "sawubona": "sah-woo-BOH-nah",
+    "ngiyabonga": "ngee-yah-BONG-gah",
+    "yebo": "YEH-boh",
+    "amanzi": "ah-MAHN-zee"
+}
 
